f5ddf58ad9a0d5a9eb37828eceeda3e5d7722d75
BIKRAM GORAI <bikram77620@gmail.com>
2026-01-07 11:48:52 +0530

    made the two schemas ->the user schema and the request schema and learnt some important code syntaxes

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-7/01/2026 - 9:10 
+7/01/2026 - 9:10 AM
 commit 1 -- Initialised the campus connect project ,made the folder structure and installed the necessary node modules
+
+
+7/01/2026 - 11:46 AM
+commit 2 -- made the two schemas ->the user schema and the request schema and learnt some important code syntaxes.
+
---
+++ models/Request.js
@@ -0,0 +1,60 @@
+import mongoose from "mongoose"
+
+const requestSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true,
+    },
+    description:{
+        type:String,
+        required:true,
+    },
+    category:{
+        type:String,
+        required:true  
+    },
+    status:{
+        type:String,
+        enum:["OPEN","ACCEPTED","COMPLETED"],
+        default:"OPEN",
+    },
+    createdBy:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true,
+    },
+
+    acceptedby:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        default:null,
+    }},
+
+    {timestamps:true}
+)
+
+
+const Request= mongoose.model("Request",requestSchema);
+export default Request;
+
+
+
+
+//What i learnt
+
+// what is enum?
+//  => so enum allows only some fixed values to be entered to the database and does not allow some random string to be entered , which make the data predictable and less buggy and prevents unexpected input
+
+//* IMPORTANT CONCEPTS
+//what is ObjectId?
+//ObjectId creates a reference to another mongoDb document.
+//by this we can directly get all the fields of that user using that reference and show details of that user 
+//What is ref:"User"?
+//ref tells mongoose which collection this ObjectId belongs to .
+
+
+
+
+//Why use Timestamps?
+// =>to keep tract of when a particular document (do not mistake it for collection) gets created and updated
+//also we can arrange the collection according to time
